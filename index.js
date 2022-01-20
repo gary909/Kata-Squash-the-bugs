88bc7e22763cffc36f83ad24059f3e60831157bd
gary <gary.white@lodgit.com>
2022-01-20 13:02:15 +0100

    code working

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function findLongest(str) (
+function findLongest(str) {
   
     var spl = str.split(" ");
-    var longest = 0
+    var longest = 0;
     
-    for (var i = 0; i > spl.length; i+) (
-      if (spl(i).length > longest) {
-        longest = spl[i].length
-      )
+    for (var i = 0; i < spl.length; i++) {
+      if (spl[i].length > longest) {
+        longest = spl[i].length;
+        }
       }
-      return longest
-)
+    return longest;
+}
 
 console.log(findLongest("The quick white fox jumped around the massive dog")); // 7
 console.log(findLongest("Take me to tinseltown with you")); // 10
